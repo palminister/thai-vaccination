d704fb7c7eac9a014ef5a00c8940512d083fddc5
Palm Jumnongrat <thanapatj.th@gmail.com>
2021-05-12 11:05:37 +0700

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Thailand's Vaccination Dashboard 💉
 
 Developed with `Next.js` + `D3.js` + `Google Sheets API` + `Tailwind JIT` + `AOS`
+
+## What's it all about? 👋
+
+This is a dashboard that monitors the vaccination process in Thailand. It is determined to visualize the hope that Thais can hold on to during this COVID-19 pandemic.
+
+## Contributors ✨
+
+This project wouldn't be possible without these wonderful people 🙏
+
+- [@lucasrodes](https://github.com/lucasrodes) --> Data 💻
+- [@padagot](https://www.facebook.com/padagott) --> Content ⌨️
+
+💉 [thai-vaccination](https://thai-vaccination.vercel.app/)
--- pages/components/Credits.js
+++ pages/components/Credits.js
@@ -11,7 +11,7 @@ const Credits = () => {
     <div className="">
       <div className="flex justify-center py-10">
         <h1
-          className="text-2xl font-anuphan text-gray-200"
+          className="text-2xl text-gray-200 font-anuphan"
           data-aos="fade"
           data-aos-duration="700"
         >
@@ -22,7 +22,7 @@ const Credits = () => {
         <div className="flex flex-wrap justify-center max-w-2xl gap-7">
           <div data-aos="fade">
             <a
-              href="https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/Thailand.csv"
+              href="https://github.com/lucasrodes"
               target="_blank"
               rel="noreferrer"
             >
@@ -34,14 +34,20 @@ const Credits = () => {
                   height="80"
                 ></Image>
               </div>
-              <p className="bg-green-700 hover:bg-opacity-30 bg-opacity-20 rounded-md m-2  text-center text-green-400 font-mono">
+            </a>
+            <a
+              href="https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/country_data/Thailand.csv"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="m-2 font-mono text-center text-green-400 bg-green-700 rounded-md hover:bg-opacity-30 bg-opacity-20">
                 Data
               </p>
             </a>
           </div>
           <div data-aos="fade">
             <a
-              href="https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSKl0LkLkxoXgA2rhVkypR2xx3hcrJoaXkkiici24mmjKomWmiRUEp-IGWsfpv5ZHn2gj0ZzRaOu2T2/pubhtml#"
+              href="https://www.facebook.com/padagott"
               target="_blank"
               rel="noreferrer"
             >
@@ -53,7 +59,13 @@ const Credits = () => {
                   height="80"
                 ></Image>
               </div>
-              <p className="bg-green-700 hover:bg-opacity-30 bg-opacity-20 w-24 m-2 rounded-md text-center text-green-400 font-mono">
+            </a>
+            <a
+              href="https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSKl0LkLkxoXgA2rhVkypR2xx3hcrJoaXkkiici24mmjKomWmiRUEp-IGWsfpv5ZHn2gj0ZzRaOu2T2/pubhtml#"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className="w-24 m-2 font-mono text-center text-green-400 bg-green-700 rounded-md hover:bg-opacity-30 bg-opacity-20">
                 Content
               </p>
             </a>
